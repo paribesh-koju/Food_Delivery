bbf4e0d2ee2af30638c7cb1eb01f7fb2e61caa36
paribesh-koju <kojuparibesh1234@gmail.com>
2023-07-14 22:22:13 +0545

    testing works under user, admin and product

--- src/test/java/com/example/annna_bazar/UserTesting.java
+++ src/test/java/com/example/annna_bazar/UserTesting.java
@@ -10,6 +10,10 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+import java.util.Optional;
+
 @DataJpaTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UserTesting {
@@ -25,12 +29,12 @@ public class UserTesting {
 
         User employee = User.builder()
 
-                .fullName("Ramesh")
+                .fullName("Paribesh")
 
-                .password("Fadatare")
+                .password("aasu")
 
-                .email("ramesh@gmail.com")
-                .mobileNo("87654")
+                .email("koju@gmail.com")
+                .mobileNo("6487654")
 
                 .build();
 
@@ -42,5 +46,48 @@ public class UserTesting {
 
         Assertions.assertThat(employee.getId()).isGreaterThan(0);
 
+    }
+    @Test
+    @Order(2)
+    public  void getCustomerTest(){
+        User customerCreated= userRepo.findById(1).get();
+        Assertions.assertThat(customerCreated.getId()).isEqualTo(1);
+    }
+
+    @Test
+    @Order(3)
+    public void getListOfCustomerTest(){
+        List<User> users = userRepo.findAll();
+        Assertions.assertThat(users.size()).isGreaterThan(0);
+    }
+
+    @Test
+    @Order(4)
+    @Rollback(value = false)
+    public void updateCustomerTest(){
+
+        User user = userRepo.findById(1).get();
+
+        user.setFullName("Yusha Sth");
+
+        User customerUpdated =  userRepo.save(user);
+
+        Assertions.assertThat(customerUpdated.getFullName()).isEqualTo("Yusha Sth");
+
+    }
+
+    @Test
+    @Order(5)
+    @Rollback(value = false)
+    public void deleteUserTest(){
+        User user = userRepo.findById(1).get();
+        userRepo.delete(user);
+
+        User user1 = null;
+        Optional<User> opUser = userRepo.findById(1);
+        if(opUser.isPresent()){
+            user1 = opUser.get();
+        }
+        Assertions.assertThat(user1).isNull();
     }
 }
